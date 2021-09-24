a7b4c5e76ab84a2c141988def8263ebf8c013748
SRay-2020 <stephen.r.ray@gmail.com>
2021-09-24 18:15:57 +0000

    Add data to frames model, migrate changes and freeze requirements

--- prints/admin.py
+++ prints/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import PhotoPrint
 
 # Register your models here.
+
+admin.site.register(PhotoPrint)
--- prints/models.py
+++ prints/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class PhotoPrint(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.name
+
