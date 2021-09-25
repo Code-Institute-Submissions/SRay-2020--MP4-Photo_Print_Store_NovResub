54592ac80c2a0ab7bfdb3b731f0a14a9166d71bc
SRay-2020 <stephen.r.ray@gmail.com>
2021-09-25 10:15:50 +0000

    Add frames url and views to app, begin style

--- frames/views.py
+++ frames/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import PhotoFrame
 
 # Create your views here.
+
+def frames(request):
+
+    frames = PhotoFrame.objects.all()
+
+    context = {
+        'frames': frames,
+    }
+
+    return render(request, 'frames/frames.html', context)
