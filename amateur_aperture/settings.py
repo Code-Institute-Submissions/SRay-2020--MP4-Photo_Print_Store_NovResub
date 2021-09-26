72c5de73a5e55478ac9f93c044504d014a4371fb
SRay-2020 <stephen.r.ray@gmail.com>
2021-09-26 11:27:43 +0000

    Create bag app and begin to style

--- amateur_aperture/settings.py
+++ amateur_aperture/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'home',
     'prints',
     'frames',
+    'bag',
 ]
 
 MIDDLEWARE = [
--- amateur_aperture/urls.py
+++ amateur_aperture/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('frames/', include('frames.urls')),
     path('prints/', include('prints.urls')),
+    path('bag/', include('bag.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
