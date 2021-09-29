afbc0719d6a31dd79497bfb0768dfed922b16b54
SRay-2020 <stephen.r.ray@gmail.com>
2021-09-29 14:58:16 +0000

    Add product details view

--- products/views.py
+++ products/views.py
@@ -20,3 +20,13 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+def product_detail(request, product_id):
+  
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
