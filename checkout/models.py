41adb79ec8f4f5f6ac9c0441bf7ace286f53d763
SRay-2020 <stephen.r.ray@gmail.com>
2021-09-30 15:47:40 +0000

    Add bulk of Stripe functionality to project

--- checkout/models.py
+++ checkout/models.py
@@ -3,6 +3,7 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
+from decimal import Decimal
 
 from django_countries.fields import CountryField
 
@@ -25,6 +26,8 @@ class Order(models.Model):
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     original_bag = models.TextField(null=False, blank=False, default="")
+    stripe_pid = models.CharField(max_length=254,null=False, blank=False, default="")
+
     
 
     def _generate_order_number(self):
