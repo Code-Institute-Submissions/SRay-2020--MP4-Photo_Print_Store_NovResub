305780046e263706f50da3f81c1641e41e6c458f
SRay-2020 <stephen.r.ray@gmail.com>
2021-09-30 19:00:03 +0000

    Add CRUD functionality to products for admin

---
+++ products/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .widgets import CustomClearableFileInput
+from .models import Product, Department
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product 
+        fields = '__all__'
+
+    image = forms.ImageField(label="Image", required=False, widget=CustomClearableFileInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        departments = Department.objects.all()
+        display_names = [(d.id, d.get_display_name()) for d in departments]
+
+        self.fields['department'].choices = display_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
