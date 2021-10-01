c6f2ae0b3f19038ab1f43401df871228912bca3a
SRay-2020 <stephen.r.ray@gmail.com>
2021-10-01 10:20:32 +0000

    Begin deployment to heroku, reload json models

--- amateur_aperture/settings.py
+++ amateur_aperture/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 # from pathlib import Path
 import os
-
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
