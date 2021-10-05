0393ae485067b05b43433c0a9b1d0d9dfecaa6e1
SRay-2020 <stephen.r.ray@gmail.com>
2021-10-05 19:50:27 +0000

    Change the way delivery total is calculated

--- bag/contexts.py
+++ bag/contexts.py
@@ -34,7 +34,7 @@ def bag_contents(request):
                 })
 
     
-    delivery = total  * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+    delivery = total  * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100 )
 
     grand_total = delivery + total 
 
@@ -47,4 +47,3 @@ def bag_contents(request):
     }
 
     return context
-
