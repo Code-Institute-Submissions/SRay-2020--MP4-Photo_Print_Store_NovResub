247704b27e2a8e85cce54f489ac4efc6d9a29415
SRay-2020 <stephen.r.ray@gmail.com>
2021-10-05 21:40:21 +0000

    Change secret key and add hidden path to retrieve new one

--- amateur_aperture/settings.py
+++ amateur_aperture/settings.py
@@ -34,7 +34,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)afxnhh(r05ss0d572$%j@(vaksx3i56q8$vexklkf@=%ft@0m'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
